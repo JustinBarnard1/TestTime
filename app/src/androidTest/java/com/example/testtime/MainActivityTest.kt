614a5f51526b55b47efe6db09885e287c481427a
justinbarnard <jbarnard0031@gmail.com>
2021-05-16 17:50:28 -0600

    Display Tests

---
+++ app/src/androidTest/java/com/example/testtime/MainActivityTest.kt
@@ -0,0 +1,42 @@
+package com.example.testtime
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest{
+
+    @get: Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun test_isActivityVisible() {
+
+        onView(withId(R.id.main))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_visibility_title_nextButton() {
+
+        onView(withId(R.id.activity_main_title))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_next_activity))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_isTitleTextDisplayed() {
+
+        onView(withId(R.id.activity_main_title))
+            .check(matches(withText(R.string.text_mainactivity)))
+    }
+}
