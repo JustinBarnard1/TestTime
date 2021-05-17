e43d157abf0f924daa224dc4e7ccd9261e897beb
justinbarnard <jbarnard0031@gmail.com>
2021-05-16 18:19:27 -0600

    Nav and Suite

--- app/src/androidTest/java/com/example/testtime/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/testtime/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.example.testtime
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -18,25 +20,37 @@ class MainActivityTest{
 
     @Test
     fun test_isActivityVisible() {
-
         onView(withId(R.id.main))
             .check(matches(isDisplayed()))
     }
 
     @Test
     fun test_visibility_title_nextButton() {
-
         onView(withId(R.id.activity_main_title))
             .check(matches(isDisplayed()))
-
         onView(withId(R.id.button_next_activity))
             .check(matches(isDisplayed()))
     }
 
     @Test
     fun test_isTitleTextDisplayed() {
-
         onView(withId(R.id.activity_main_title))
             .check(matches(withText(R.string.text_mainactivity)))
+    }
+
+    @Test
+    fun test_navSecondaryActivity() {
+        onView(withId(R.id.button_next_activity))
+            .perform(click())
+        onView(withId(R.id.secondary))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_backPress_toMainActivity() {
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 }
